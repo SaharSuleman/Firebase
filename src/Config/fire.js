c1fed26513a30ea6ccf365d4869af6feec3e9ca5
Sabahat767 <sabahatsabir12@gmail.com>
2020-12-17 03:01:46 +0500

    Firebase Login Auth

---
+++ src/Config/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+var firebaseConfig = {
+    apiKey: "AIzaSyAf51sIRruzJ74FsmEwOxTnwXB6DKJxubg",
+    authDomain: "login-7d410.firebaseapp.com",
+    projectId: "login-7d410",
+    storageBucket: "login-7d410.appspot.com",
+    messagingSenderId: "298761837518",
+    appId: "1:298761837518:web:444e413db7e7172492e99f"
+  };
+  // Initialize Firebase
+  const fire=firebase.initializeApp(firebaseConfig);
+  export default fire;
